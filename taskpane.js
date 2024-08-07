6311558f48071027e7b25072efa99d2cc1b8cb13
anant-jaffa <anant@jaffa-ai.com>
2024-08-08 01:53:18 +0530

    changed the url

--- taskpane.js
+++ taskpane.js
@@ -20,7 +20,7 @@ async function uploadFiles() {
     formData.append('audio', audioFile);
 
     try {
-        const response = await fetch('http://127.0.0.1:5000/process', {
+        const response = await fetch('https://flask-app-5ux4.onrender.com/process', {
             method: 'POST',
             body: formData
         });
